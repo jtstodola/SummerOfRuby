799758f8aa36fd2203804dc100af5cadbf1568c9
Jessica <jtstodola@gmail.com>
2015-07-27 00:10:33 -0500

    Updated cart.rb

--- week2_homework/lib/cart.rb
+++ week2_homework/lib/cart.rb
@@ -1,39 +1,3 @@
-class Item
-  attr_accessor :title, :price
-  
-  def initialize(title, price)
-    @title = title
-    @price = price
-  end
-end
-
-class Library
-  attr_reader :books, :name, :quantity
-  
-  def initialize
-    @books = []
-    @books << Item.new("Pragmatic Programmer\t", 32)
-    @books << Item.new("Web Development Recipes", 24)
-    @books << Item.new("Hello, Android\t", 25)
-    @books << Item.new("iOS 8 SDK Development", 25)
-    @books << Item.new("Programming Ruby 1.9 & 2.0", 28)
-  end
-  
-  def display_menu
-    output = ""
-    
-    @books.each_with_index {|book, index| 
-      output << "#{index + 1}. #{book.title}\t$#{"%.2f" % book.price}\n"}
-    output
-  end
-end
-  
-class OutsideParametersError < StandardError
-  def to_s
-    "Please enter a number between 1 - 5."
-  end
-end
-
 class Cart 
   attr_accessor :total
   attr_reader :cart, :items, :item
@@ -79,37 +43,3 @@ class Cart
   
 end
 
-# Goes into app.rb
-library = Library.new
-cart = Cart.new
-puts library.display_menu
-
-
-keep_going = true
-while(keep_going) do
-  puts "Please choose an item (1 - 5) to add to your Cart: \n"
-  book_choice = gets.chop
-  book_choice = book_choice.to_i
-  
-  incorrect_choice = true
-  while(incorrect_choice) do
-    if (book_choice > 0 && book_choice < 6 && book_choice.to_s != book_choice)
-      book_choice = library.books[book_choice - 1]
-      book_choice = book_choice.title
-      cart.add_item(book_choice, 1)
-      incorrect_choice = false
-    else
-      puts "Please choose a number between 1 - 5"
-      book_choice = gets.chop
-      book_choice = book_choice.to_i
-    end
-  end
-        
-  puts "Would you like to add another item?"
-  keep_going = gets.chop
-  keep_going = keep_going == "y" || keep_going == "Y"
-end
-
-puts "Items in your Cart: #{cart.total}" 
-puts cart.items
-#puts"Total price: ${"%.2f" % total_price}"
